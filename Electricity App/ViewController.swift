f28c65ee46ca55168ab673c1d5e8ef04437467bf
Dators <rzelenke@Datorss-iMac.lan>
2015-07-12 22:14:43 +0300

    Initial Commit

---
+++ Electricity App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Electricity App
+//
+//  Created by Dators on 12/07/15.
+//  Copyright (c) 2015 AppDevCenter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
